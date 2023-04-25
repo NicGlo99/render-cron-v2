942c4a43c261d6cf9c41bb8e3505a9d9860d68cd
Daniele <danielericcelli@me.com>
2023-04-25 20:46:48 +0200

    some fix

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const CronJob = require('cron').CronJob;
+const express = require('express');
+const app = express();
 
 function myFunc() {
     console.log('Cron job executed!');
 }
 
-const job = new CronJob('*/1 * * * *', myFunc);
+const job = new CronJob('0 10 * * *', myFunc);
 
 job.start();
+
+app.get('/healthcheck', (req, res) => {
+    // Verifica lo stato del server qui
+    if (job.running) {
+        res.status(200).send('Server is running');
+    } else {
+        res.status(500).send('Server is not running');
+    }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
